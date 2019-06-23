ac68bf2b4e003be22a8e0e63d5c7b33f13a79aff
yoraish <yorai.shaoul@gmail.com>
2019-06-22 21:19:27 -0400

    implementing message receiving

--- comm-server.py
+++ comm-server.py
@@ -5,6 +5,7 @@ import cgi
 import json
 import datetime
 import sqlite3
+import datetime
 cgitb.enable()
 """
 # related files are:
@@ -16,19 +17,39 @@ cgitb.enable()
 
 DEFAULT_MSG = "I'm shouting and barfing hysterically"
 
-def update_msgs(data):
+def receive_msgs_to_hist_and_dict(data):
     """
     function that updates the file msg_history.txt with a new msg request
     Arguments:
-        data {} -- data of form {"sender": string_sender_name}
+        data {} -- data of form {"sender": string_sender_name, "password":123456}
     """
     # take the msg that corresponds to the name in the json file, revert it to default for future use, and send the msg to msg_history.txt
     # if name is not in the json file, then add it and assign default msg
     with open("name_to_msg.json", 'r') as name_to_msg_data:
         name_to_msg = json.load(name_to_msg_data)
-        print("\n")
+
+        print("<br> Name to msg===\n")
         print(name_to_msg)
-        print("<br>", data["sender"].value)
+        print("<br>Sender name=", data["sender"].value)
+        sender_name = data["sender"]
+        # if the sender is in the dict, then use the msg that's mapped there
+        if sender_name in name_to_msg:
+            msg_to_send = name_to_msg[sender_name]
+        else:
+            msg_to_send = DEFAULT_MSG
+        # in any case, revert back the msg in the dict to the default now
+        # this also adds the sender if it didn't exist in the dict
+        name_to_msg[sender_name] = DEFAULT_MSG
+
+    # now actually send the msg (write to text file)
+    with open("msg_history.txt", "a") as hist_file:
+        timestamp = datetime.datetime.now()
+        hist_file.write(sender_name +"@"+ str(timestamp.hour)+":"+str(timestamp.minute) +" " + msg_to_send )
+
+        # now clients would be able to grab the last lines of the txt file to get updated messages
+
+
+
         
     
     
@@ -40,4 +61,4 @@ if __name__ == "__main__":
     #command below takes the arguements from the get request and puts them in some sort of a dictionary
     inDataDict = cgi.FieldStorage()
     # update the msg_history.txt file with the new msg
-    update_msgs(inDataDict)
+    receive_msgs_to_hist_and_dict(inDataDict)
